200dd3ff876a67536115ddbe89a0b286742f50ac
marta00084 <martvonk00084@gmail.com>
2023-12-18 17:43:57 +0100

    update

--- remix.config.js
+++ remix.config.js
@@ -1,24 +1,24 @@
-import { flatRoutes } from "remix-flat-routes";
+import { flatRoutes } from 'remix-flat-routes'
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 export default {
-  ignoredRouteFiles: ["**/*"],
-  tailwind: true,
-  postcss: true,
-  watchPaths: ["./tailwind.config.ts"],
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildPath: "build/index.js",
-  routes: async (defineRoutes) => {
-    return flatRoutes("routes", defineRoutes, {
-      ignoredRouteFiles: [
-        ".*",
-        "**/*.css",
-        "**/*.test.{js,jsx,ts,tsx}",
-        "**/__*.*",
-      ],
-    });
-  },
-};
+	ignoredRouteFiles: ['**/*'],
+	tailwind: true,
+	postcss: true,
+	watchPaths: ['./tailwind.config.ts'],
+	appDirectory: 'app',
+	assetsBuildDirectory: 'public/build',
+	publicPath: '/build/',
+	serverBuildPath: 'build/index.js',
+	routes: async defineRoutes => {
+		return flatRoutes('routes', defineRoutes, {
+			ignoredRouteFiles: [
+				'.*',
+				'**/*.css',
+				'**/*.test.{js,jsx,ts,tsx}',
+				'**/__*.*',
+			],
+		})
+	},
+}
