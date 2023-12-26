11f9b62124f79af22d681992e920407e20f5132a
marta00084 <martvonk00084@gmail.com>
2023-12-26 03:23:48 +0100

    pp

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,78 +1,38 @@
-import { Link } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/node'
+import { prisma } from '~/utils/db.server'
+import { getUserImgSrc } from '~/utils/misc'
 
-export default function index() {
+export async function loader() {
+	const users = await prisma.user.findMany({
+		select: {
+			name: true,
+			username: true,
+			image: { select: { id: true } },
+		},
+	})
+
+	return json(users)
+}
+
+export default function Index() {
+	const users =
+		useLoaderData<
+			{ username: string; name: string | null; image: { id: string } | null }[]
+		>()
+
 	return (
 		<main className="flex-grow py-4 px-4 sm:px-6 lg:px-8 bg-gray-100 ">
 			<div>
-				<div className="bg-white shadow-sm rounded-lg overflow-hidden m-4 p-8 text-lg font-bold w-48">
-					<Link to="/users/marta">User: marta</Link>
-				</div>
-			</div>
-
-			<form action="/login" method="POST" className="hidden">
-				<label>
-					{' '}
-					Username:
-					<input
-						className="border-2 border-gray-400"
-						placeholder="username"
-						name="username"
-					/>
-				</label>
-				<label>
-					Password:
-					<input
-						className="border-2 border-gray-400"
-						placeholder="password"
-						name="password"
-						type="password"
-					/>
-				</label>
-				<button type="submit">Login</button>
-			</form>
-
-			<div className="hidden max-w-3xl mx-auto">
-				<h1 className="text-2xl font-bold text-gray-900 mb-8 mx-2">Posts</h1>
-				<div className="bg-white shadow-sm rounded-lg overflow-hidden mt-4">
-					<div className="p-2">
-						<div className="flex justify-between items-center">
-							<h1 className="text-xl font-bold text-gray-900">
-								Title of the Blog Post
-							</h1>
-							<p className="text-gray-600">2 hours ago</p>
-						</div>
+				{users.map(user => (
+					<div
+						key={user.name}
+						className="bg-white shadow-sm rounded-lg overflow-hidden m-4 p-8 text-lg font-bold w-48"
+					>
+						<img src={getUserImgSrc(user.image?.id)} alt={user.name || ''} />
+						<Link to={`/users/${user.username}`}>User: {user.name}</Link>
 					</div>
-				</div>
-				<div className="bg-white shadow-sm rounded-lg overflow-hidden mt-4">
-					<div className="p-2">
-						<div className="flex justify-between items-center">
-							<h1 className="text-xl font-bold text-gray-900">
-								Title of the Blog Post
-							</h1>
-							<p className="text-gray-600">2 hours ago</p>
-						</div>
-					</div>
-				</div>
-				<div className="bg-white shadow-sm rounded-lg overflow-hidden mt-4">
-					<div className="p-2">
-						<div className="flex justify-between items-center">
-							<h1 className="text-xl font-bold text-gray-900">
-								Title of the Blog Post
-							</h1>
-							<p className="text-gray-600">2 hours ago</p>
-						</div>
-					</div>
-				</div>
-				<div className="bg-white shadow-sm rounded-lg overflow-hidden mt-4">
-					<div className="p-2">
-						<div className="flex justify-between items-center">
-							<h1 className="text-xl font-bold text-gray-900">
-								Title of the Blog Post
-							</h1>
-							<p className="text-gray-600">2 hours ago</p>
-						</div>
-					</div>
-				</div>
+				))}
 			</div>
 		</main>
 	)
